f977187031c8908028f20d7ac942cd0550e4588b
ian <ian>
2020-07-09 16:46:45 -0300

    first

---
+++ src/main/java/br/com/spdata/TestePraticoApplication.java
@@ -0,0 +1,13 @@
+package br.com.spdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestePraticoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestePraticoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/spdata/TestePraticoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.spdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestePraticoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
