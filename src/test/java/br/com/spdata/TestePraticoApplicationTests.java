7b538bc8653ddbbca977745c5a995ee117ea6099
Diogobh93 <diogobh93@gmail.com>
2020-07-20 17:05:25 -0300

    Test sucess

--- src/test/java/br/com/spdata/TestePraticoApplicationTests.java
+++ src/test/java/br/com/spdata/TestePraticoApplicationTests.java
@@ -8,6 +8,13 @@ class TestePraticoApplicationTests {
 
 	@Test
 	void contextLoads() {
+		int numero = 2;
+		
+		if(numero % 2 == 0) {
+			System.out.print("O número é par ");
+		} else {
+			System.out.print("O número é impar ");
+		}
 	}
 
 }
